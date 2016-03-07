94a7cb5d9403e93616ebdb8a01a0393f25361af5
manuke <gomimailhoihoi@gmail.com>
2016-03-07 13:00:55 +0900

    path

--- ws_client.php
+++ ws_client.php
@@ -3,27 +3,98 @@ require('vendor/autoload.php');
 use WebSocket\Client;
 $client = new Client("ws://127.0.0.1:8081/");
 
+
+
+$arrays =
+    array(
+        array(
+            "qid"=>"1",
+            "kind"=>"question",
+            "question"=>"結婚おめでとう!abc",
+            "A1"=>"kaiou1",
+            "A2"=>"kaiou2",
+            "A3"=>"kaiou3",
+            "A4"=>"kaiou4",
+            'image'=>'',
+        ),
+        array(
+            "qid"=>"2",
+            "kind"=>"question",
+            "question"=>"結婚おめでとう!abc2",
+            "A1"=>"kaiou1",
+            "A2"=>"kaiou2",
+            "A3"=>"kaiou3",
+            "A4"=>"kaiou4",
+            'image'=>'',
+        ),
+        array(
+            "qid"=>"3",
+            "kind"=>"question",
+            "question"=>"3",
+            "A1"=>"kaiou1",
+            "A2"=>"kaiou2",
+            "A3"=>"kaiou3",
+            "A4"=>"kaiou4",
+            'image'=>'/tmp/googlelogo_color_272x92dp.png',
+        ),
+    );
+
+send($arrays, 2);
+exit;
+function send($arrays, $id)
+{
+    $client = new Client("ws://127.0.0.1:8081/");
+    $json = json_encode(array(
+        "countdown"=>"countdown",
+        "kind"=>"countdown",
+    ));
+    $client->send($json);
+    sleep(3);
+    $array = $arrays[$id];
+    if ($array['question']) {
+        $client->send(json_encode($array));
+    }
+    sleep(3);
+    if ($array['image']) {
+        $file = file_get_contents($array['image']);
+        $client->send($file, "binary");
+    }
+}
+
+
 //TEXT
 $json = json_encode(array(
-"qid"=>"2", 
-"kind"=>"question",
-"question"=>"結婚おめでとう!abc",
-"A1"=>"kaiou1",
-"A2"=>"kaiou2",
-"A3"=>"kaiou3",
-"A4"=>"kaiou4"
+    "qid"=>"2",
+    "kind"=>"question",
+    "question"=>"結婚おめでとう!abc",
+    "A1"=>"kaiou1",
+    "A2"=>"kaiou2",
+    "A3"=>"kaiou3",
+    "A4"=>"kaiou4"
 ));
-$client->send($json);
+//$client->send($json);
 
-exit;
+//URL
+$json = json_encode(array(
+    "url"=>"table.html",
+    "kind"=>"url",
+));
+//$client->send($json);
+
+//COUNTDOWN
+$json = json_encode(array(
+    "countdown"=>"countdown",
+    "kind"=>"countdown",
+));
+//$client->send($json);
 
 //BINARY
-$dir = '/Users/ka/Desktop/';
+$dir = '/tmp/images/';
 $files = glob(rtrim($dir, '/') . '/*.jpg');
 for ($i=0 ; $i<100000 ; $i++) {
-foreach ($files as $file) {
-$file = file_get_contents($file);
-$client->send($file, "binary");
-sleep(3);
-}
+    foreach ($files as $file) {
+        $file = file_get_contents($file);
+        $client->send($file, "binary");
+        sleep(3);
+    }
 }
